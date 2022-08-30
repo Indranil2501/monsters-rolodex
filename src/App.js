faefc0a46695e373f2fe5943ca41518afb7b481e
indranil2501 <indranil2501@gmail.com>
2022-08-30 12:14:32 +0530

    Added component state

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name: "Indranil"
+    }
+  }
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn ReactJS
-          </a>
+          <p>Hi {this.state.name}</p>
+          <button>Change Names</button>
         </header>
       </div>
     );
