b37db118fea900ab14d85e6d100eac8d2b0d3f76
indranil2501 <indranil2501@gmail.com>
2022-09-01 13:07:04 +0530

    Used componentDidMount to fetch api response

--- src/App.js
+++ src/App.js
@@ -7,29 +7,29 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Linda"
-        },
-        {
-          name: "John"
-        },
-        {
-          name: "Johnny"
-        },
-        {
-          name: "Nil"
-        }
-      ]
+      monsters: []
     }
   }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState(()=>{
+        return {monsters: users}
+      }, ()=>{
+        console.log(this.state);
+      }))
+  }
+
   render() {
     return (
       <div className="App">
         {
           this.state.monsters.map((monster)=>{
             return(
-              <h1>{monster.name}</h1>
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
             );
           })
         }
